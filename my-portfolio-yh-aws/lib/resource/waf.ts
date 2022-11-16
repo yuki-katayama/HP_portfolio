ee13cec9088d44efb52681031fd67bbf1e1eacde
yuki-katayama <katayu810@gmail.com>
2022-11-16 21:05:01 +0900

    不要なフォルダを削除

--- my-portfolio-yh-aws/lib/resource/waf.ts
+++
@@ -1,8 +0,0 @@
-
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as waf from 'aws-cdk-lib/aws-waf';
-import * as targets from 'aws-cdk-lib/aws-route53-targets';
-
-export class Waf {
-}
